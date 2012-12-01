c6c826590de79baefdc3c4af979b9762be5b64a5
cjmersm <cjmersm@rams.colostate.edu>
2012-12-01 14:30:32 -0700

    Branched

--- PrintStuff.java
+++ PrintStuff.java
@@ -1,6 +1,10 @@
 
 public class PrintStuff {
 	public static void main(String[] args){
-		System.out.println("Stuff");
+		PrinterObject po = new PrinterObject("This is");
+		PrinterObject po2= new PrinterObject("Source Control");
+		
+		po.print();
+		po2.print();
 	}
 }
---
+++ PrinterObject.java
@@ -0,0 +1,12 @@
+
+public class PrinterObject {
+	private String str;
+	
+	PrinterObject(String str){
+		this.str = str;
+	}
+	
+	public void print(){
+		System.out.println(str);
+	}
+}
